8325277be5fec9908aa959fedc83fb3e461db3d0
DOMINO <tvaishnavi513@gmail.com>
2026-06-21 22:28:54 +0530

    Create turtle.py

---
+++ python_code/turtle.py
@@ -0,0 +1,13 @@
+import turtle as t
+t.speed(0)
+for i in range (4):
+    t.forward(100)
+    t.left(90)
+
+t.circle(100)
+t.clearscreen()
+t.speed(0)
+for i in range(36):
+    t.circle(100)
+    t.left(10)
+t.clearscreen()
